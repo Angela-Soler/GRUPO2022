a3ed58e79c333cb2e436682d25500a3d5f909a6e
user <user@DESKTOP-HO932NV>
2022-07-14 21:53:57 -0600

    metodos establecer y obtener de la clase Doctor

--- src/Clases/Doctor.java
+++ src/Clases/Doctor.java
@@ -8,6 +8,41 @@ package Clases;
  *
  * @author admin
  */
-public class Doctor {
+public class Doctor extends Persona {
+    private String estudio;
+    private String hospital;
+    
+    @Override
+    String getNombre() {
+        return("Marlon Noe Nieto");
+    }
+
+    @Override
+    int getEdad() {
+        return (30);
+    }
+
+    @Override
+    String getEspecialidad() {
+        return("Neurocirujano");
+    }
+    
+    //metodos establecer de la variables
+    public void establecerEstudio(String _estudio){
+        this.estudio = _estudio;
+    }
+    
+    public void establecerHospital(String _hospital){
+        this.hospital = _hospital;
+    }
+    
+    //metodos obtener de la variables
+    public String obtenerEstudio(){
+        return estudio;
+    }
+    
+    public String obtenerHospital(){
+        return hospital;
+    }
     
 }
