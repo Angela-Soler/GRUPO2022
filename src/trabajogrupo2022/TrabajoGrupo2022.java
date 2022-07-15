b922cb4d40770372b7656bb927d634dae5639929
user <user@DESKTOP-HO932NV>
2022-07-14 20:39:32 -0600

    Primer commit del repositorio

---
+++ src/trabajogrupo2022/TrabajoGrupo2022.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajogrupo2022;
+
+/**
+ *
+ * @author user
+ */
+public class TrabajoGrupo2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            System.out.println("Trabajo Grupal. Programación Orientada a Objetos 2022");
+    }
+    
+}
