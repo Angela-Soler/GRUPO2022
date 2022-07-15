1da05ca714c43e9b63076a4285a486f93e6125d2
user <user@DESKTOP-HO932NV>
2022-07-14 22:00:21 -0600

    creamos los metodos para la clase deportista

--- src/Clases/Deportista.java
+++ src/Clases/Deportista.java
@@ -8,6 +8,41 @@ package Clases;
  *
  * @author user
  */
-public class Deportista {
+public class Deportista extends Persona {
     
+    private String tipoNados;
+    private String competiciones;
+    
+     @Override
+    String getNombre() {
+        return("Katherin Trochez");
+    }
+
+    @Override
+    int getEdad() {
+        return (25);
+    }
+
+    @Override
+    String getEspecialidad() {
+        return("Natacion");
+    }
+    
+    //metodos establecer de la variables
+    public void establecerTipoNados(String _tipoNados){
+        this.tipoNados = _tipoNados;
+    }
+    
+    public void establecerCompeticiones(String _competiciones){
+        this.competiciones = _competiciones;
+    }
+    
+    //metodos obtener de la variables
+    public String obtenerTipoNados(){
+        return tipoNados;
+    }
+    
+    public String obtenerCompeticiones(){
+        return competiciones;
+    }
 }
